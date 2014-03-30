e507ba431b9ebfd966ac00c5f8120ffde50d15bf
toufu <465253100@qq.com>
2014-03-30 19:44:43 +0800

    登录界面/个人信息页面

---
+++ 原型/prototype/LoginForm.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace prototype
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.AccountTextBox = new System.Windows.Forms.TextBox();
+            this.noticeCheckBox = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("幼圆", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(113, 85);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(447, 35);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "初次使用，请输入你的信息";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("宋体", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(158, 191);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 21);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "账号：";
+            // 
+            // AccountTextBox
+            // 
+            this.AccountTextBox.Font = new System.Drawing.Font("宋体", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.AccountTextBox.Location = new System.Drawing.Point(223, 183);
+            this.AccountTextBox.Name = "AccountTextBox";
+            this.AccountTextBox.Size = new System.Drawing.Size(216, 29);
+            this.AccountTextBox.TabIndex = 2;
+            this.AccountTextBox.UseWaitCursor = true;
+            this.AccountTextBox.WordWrap = false;
+            // 
+            // noticeCheckBox
+            // 
+            this.noticeCheckBox.AutoSize = true;
+            this.noticeCheckBox.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.noticeCheckBox.Location = new System.Drawing.Point(223, 244);
+            this.noticeCheckBox.Name = "noticeCheckBox";
+            this.noticeCheckBox.Size = new System.Drawing.Size(139, 20);
+            this.noticeCheckBox.TabIndex = 3;
+            this.noticeCheckBox.Text = "已阅读使用须知";
+            this.noticeCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("幼圆", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button1.Location = new System.Drawing.Point(223, 299);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "GO";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(675, 419);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.noticeCheckBox);
+            this.Controls.Add(this.AccountTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "LoginForm";
+            this.Text = "谁与争锋";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox AccountTextBox;
+        private System.Windows.Forms.CheckBox noticeCheckBox;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ 原型/prototype/PersonalInfoForm.Designer.cs
@@ -0,0 +1,261 @@
+﻿namespace prototype
+{
+    partial class PersonalInfoForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PersonalInfoForm));
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微软雅黑", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(300, 84);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(94, 46);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(118, 144);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("微软雅黑", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.button1.Location = new System.Drawing.Point(439, 199);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(144, 37);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "旧的回忆";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("微软雅黑", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.button2.Location = new System.Drawing.Point(439, 285);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(144, 37);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "学习牛人";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("微软雅黑", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.button3.Location = new System.Drawing.Point(439, 371);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(144, 37);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "荐题系统";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Calibri", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(44, 249);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(169, 36);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Information:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("宋体", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.Location = new System.Drawing.Point(65, 313);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Submits:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("宋体", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label4.Location = new System.Drawing.Point(55, 361);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(99, 20);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Accepted:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("宋体", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label5.Location = new System.Drawing.Point(171, 313);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(49, 20);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "1024";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("宋体", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label6.Location = new System.Drawing.Point(171, 361);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(39, 20);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "512";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Calibri", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(44, 422);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(212, 36);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "Solve problems:";
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Items.AddRange(new object[] {
+            "1024, 2048, 4096, 1024, 2048, 4096, 1024, 2048, 4096, 1024, 2048, 4096, 1024, 204" +
+                "8, 4096,",
+            "1111, 2222, 3333, 1024, 2048, 4096, 1024, 2048, 4096, 1024, 2048, 4096, 1024, 204" +
+                "8, 4096,",
+            "1024, 2048, 4096, 1024, 2048, 4096, 1024, 2048, 4096, 1024, 2048, 4096, 1024, 204" +
+                "8, 4096,",
+            "1111, 2222, 3333, 1024, 2048, 4096, 1024, 2048, 4096, 1024, 2048, 4096, 1024, 204" +
+                "8, 4096,",
+            "1024, 2048, 4096, 1024, 2048, 4096, 1024, 2048, 4096, 1024, 2048, 4096, 1024, 204" +
+                "8, 4096,",
+            "1111, 2222, 3333, 1024, 2048, 4096, 1024, 2048, 4096, 1024, 2048, 4096, 1024, 204" +
+                "8, 4096,",
+            "1024, 2048, 4096, 1024, 2048, 4096, 1024, 2048, 4096, 1024, 2048, 4096, 1024, 204" +
+                "8, 4096,",
+            "1111, 2222, 3333, 1024, 2048, 4096, 1024, 2048, 4096, 1024, 2048, 4096, 1024, 204" +
+                "8, 4096,",
+            "1024, 2048, 4096, 1024, 2048, 4096, 1024, 2048, 4096, 1024, 2048, 4096, 1024, 204" +
+                "8, 4096,",
+            "1111, 2222, 3333, 1024, 2048, 4096, 1024, 2048, 4096, 1024, 2048, 4096, 1024, 204" +
+                "8, 4096,",
+            "1024, 2048, 4096, 1024, 2048, 4096, 1024, 2048, 4096, 1024, 2048, 4096, 1024, 204" +
+                "8, 4096,",
+            "1111, 2222, 3333, 1024, 2048, 4096, 1024, 2048, 4096, 1024, 2048, 4096, 1024, 204" +
+                "8, 4096,",
+            "1111, 2222, 3333, 1024, 2048, 4096, 1024, 2048, 4096, 1024, 2048, 4096, 1024, 204" +
+                "8, 4096,",
+            "1024, 2048, 4096, 1024, 2048, 4096, 1024, 2048, 4096, 1024, 2048, 4096, 1024, 204" +
+                "8, 4096,",
+            "1111, 2222, 3333, 1024, 2048, 4096, 1024, 2048, 4096, 1024, 2048, 4096, 1024, 204" +
+                "8, 4096,",
+            "1024, 2048, 4096, 1024, 2048, 4096, 1024, 2048, 4096, 1024, 2048, 4096, 1024, 204" +
+                "8, 4096,",
+            "1111, 2222, 3333, 1024, 2048, 4096, 1024, 2048, 4096, 1024, 2048, 4096, 1024, 204" +
+                "8, 4096,",
+            "1024, 2048, 4096, 1024, 2048, 4096, 1024, 2048, 4096, 1024, 2048, 4096, 1024, 204" +
+                "8, 4096,",
+            "1111, 2222, 3333, 1024, 2048, 4096, 1024, 2048, 4096, 1024, 2048, 4096, 1024, 204" +
+                "8, 4096,",
+            "1024, 2048, 4096, 1024, 2048, 4096, 1024, 2048, 4096, 1024, 2048, 4096, 1024, 204" +
+                "8, 4096,",
+            "1111, 2222, 3333, 1024, 2048, 4096, 1024, 2048, 4096, 1024, 2048, 4096, 1024, 204" +
+                "8, 4096,",
+            "1024, 2048, 4096, 1024, 2048, 4096, 1024, 2048, 4096, 1024, 2048, 4096, 1024, 204" +
+                "8, 4096,",
+            "1111, 2222, 3333, 1024, 2048, 4096, 1024, 2048, 4096, 1024, 2048, 4096, 1024, 204" +
+                "8, 4096,"});
+            this.listBox1.Location = new System.Drawing.Point(69, 519);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(462, 212);
+            this.listBox1.TabIndex = 11;
+            // 
+            // PersonalInfoForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(661, 784);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "PersonalInfoForm";
+            this.Text = "PersonalInfoForm";
+            this.Load += new System.EventHandler(this.PersonalInfoForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
