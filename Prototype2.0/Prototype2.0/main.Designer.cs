8eb644fad9ce4b116ec69a6154f75c50f576a025
edisond <edisond@qq.com>
2014-04-13 20:20:02 +0800

    原型2.0

---
+++ Prototype2.0/Prototype2.0/main.Designer.cs
@@ -0,0 +1,296 @@
+﻿namespace Prototype2._0
+{
+    partial class main
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(main));
+            this.panel_menu = new System.Windows.Forms.Panel();
+            this.button_jiantixitong = new System.Windows.Forms.Button();
+            this.button_niurenduibi = new System.Windows.Forms.Button();
+            this.button_wodeliangdian = new System.Windows.Forms.Button();
+            this.button_zuotifenlei = new System.Windows.Forms.Button();
+            this.button_fendoushi = new System.Windows.Forms.Button();
+            this.button_xuexiniuren = new System.Windows.Forms.Button();
+            this.button_jiudehuiyi = new System.Windows.Forms.Button();
+            this.button_wodexinxi = new System.Windows.Forms.Button();
+            this.button_settings = new System.Windows.Forms.Button();
+            this.button_exit = new System.Windows.Forms.Button();
+            this.panel_wodexinxi = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel_jiudehuiyi = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel_xuexiniuren = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel_menu.SuspendLayout();
+            this.panel_wodexinxi.SuspendLayout();
+            this.panel_jiudehuiyi.SuspendLayout();
+            this.panel_xuexiniuren.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel_menu
+            // 
+            this.panel_menu.BackColor = System.Drawing.Color.Transparent;
+            this.panel_menu.Controls.Add(this.button_jiantixitong);
+            this.panel_menu.Controls.Add(this.button_niurenduibi);
+            this.panel_menu.Controls.Add(this.button_wodeliangdian);
+            this.panel_menu.Controls.Add(this.button_zuotifenlei);
+            this.panel_menu.Controls.Add(this.button_fendoushi);
+            this.panel_menu.Controls.Add(this.button_xuexiniuren);
+            this.panel_menu.Controls.Add(this.button_jiudehuiyi);
+            this.panel_menu.Controls.Add(this.button_wodexinxi);
+            this.panel_menu.Location = new System.Drawing.Point(21, 48);
+            this.panel_menu.Name = "panel_menu";
+            this.panel_menu.Size = new System.Drawing.Size(143, 425);
+            this.panel_menu.TabIndex = 0;
+            // 
+            // button_jiantixitong
+            // 
+            this.button_jiantixitong.FlatAppearance.BorderSize = 0;
+            this.button_jiantixitong.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_jiantixitong.Location = new System.Drawing.Point(56, 313);
+            this.button_jiantixitong.Name = "button_jiantixitong";
+            this.button_jiantixitong.Size = new System.Drawing.Size(75, 23);
+            this.button_jiantixitong.TabIndex = 7;
+            this.button_jiantixitong.Text = "荐题系统";
+            this.button_jiantixitong.UseVisualStyleBackColor = true;
+            // 
+            // button_niurenduibi
+            // 
+            this.button_niurenduibi.FlatAppearance.BorderSize = 0;
+            this.button_niurenduibi.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_niurenduibi.Location = new System.Drawing.Point(56, 284);
+            this.button_niurenduibi.Name = "button_niurenduibi";
+            this.button_niurenduibi.Size = new System.Drawing.Size(75, 23);
+            this.button_niurenduibi.TabIndex = 6;
+            this.button_niurenduibi.Text = "牛人对比";
+            this.button_niurenduibi.UseVisualStyleBackColor = true;
+            // 
+            // button_wodeliangdian
+            // 
+            this.button_wodeliangdian.FlatAppearance.BorderSize = 0;
+            this.button_wodeliangdian.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_wodeliangdian.Location = new System.Drawing.Point(56, 402);
+            this.button_wodeliangdian.Name = "button_wodeliangdian";
+            this.button_wodeliangdian.Size = new System.Drawing.Size(75, 23);
+            this.button_wodeliangdian.TabIndex = 5;
+            this.button_wodeliangdian.Text = "我的亮点";
+            this.button_wodeliangdian.UseVisualStyleBackColor = true;
+            // 
+            // button_zuotifenlei
+            // 
+            this.button_zuotifenlei.FlatAppearance.BorderSize = 0;
+            this.button_zuotifenlei.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_zuotifenlei.Location = new System.Drawing.Point(56, 372);
+            this.button_zuotifenlei.Name = "button_zuotifenlei";
+            this.button_zuotifenlei.Size = new System.Drawing.Size(75, 23);
+            this.button_zuotifenlei.TabIndex = 4;
+            this.button_zuotifenlei.Text = "做题分类";
+            this.button_zuotifenlei.UseVisualStyleBackColor = true;
+            // 
+            // button_fendoushi
+            // 
+            this.button_fendoushi.FlatAppearance.BorderSize = 0;
+            this.button_fendoushi.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_fendoushi.Location = new System.Drawing.Point(56, 342);
+            this.button_fendoushi.Name = "button_fendoushi";
+            this.button_fendoushi.Size = new System.Drawing.Size(75, 23);
+            this.button_fendoushi.TabIndex = 3;
+            this.button_fendoushi.Text = "奋斗史";
+            this.button_fendoushi.UseVisualStyleBackColor = true;
+            // 
+            // button_xuexiniuren
+            // 
+            this.button_xuexiniuren.FlatAppearance.BorderSize = 0;
+            this.button_xuexiniuren.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_xuexiniuren.Image = ((System.Drawing.Image)(resources.GetObject("button_xuexiniuren.Image")));
+            this.button_xuexiniuren.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button_xuexiniuren.Location = new System.Drawing.Point(11, 142);
+            this.button_xuexiniuren.Name = "button_xuexiniuren";
+            this.button_xuexiniuren.Size = new System.Drawing.Size(120, 40);
+            this.button_xuexiniuren.TabIndex = 2;
+            this.button_xuexiniuren.Text = "学习牛人";
+            this.button_xuexiniuren.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button_xuexiniuren.UseVisualStyleBackColor = true;
+            this.button_xuexiniuren.Click += new System.EventHandler(this.button_xuexiniuren_Click);
+            // 
+            // button_jiudehuiyi
+            // 
+            this.button_jiudehuiyi.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button_jiudehuiyi.FlatAppearance.BorderSize = 0;
+            this.button_jiudehuiyi.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_jiudehuiyi.Image = ((System.Drawing.Image)(resources.GetObject("button_jiudehuiyi.Image")));
+            this.button_jiudehuiyi.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button_jiudehuiyi.Location = new System.Drawing.Point(11, 84);
+            this.button_jiudehuiyi.Name = "button_jiudehuiyi";
+            this.button_jiudehuiyi.Size = new System.Drawing.Size(120, 40);
+            this.button_jiudehuiyi.TabIndex = 1;
+            this.button_jiudehuiyi.Text = "旧的回忆";
+            this.button_jiudehuiyi.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button_jiudehuiyi.UseVisualStyleBackColor = true;
+            this.button_jiudehuiyi.Click += new System.EventHandler(this.button_jiudehuiyi_Click);
+            // 
+            // button_wodexinxi
+            // 
+            this.button_wodexinxi.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button_wodexinxi.FlatAppearance.BorderSize = 0;
+            this.button_wodexinxi.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_wodexinxi.Image = ((System.Drawing.Image)(resources.GetObject("button_wodexinxi.Image")));
+            this.button_wodexinxi.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button_wodexinxi.Location = new System.Drawing.Point(11, 25);
+            this.button_wodexinxi.Name = "button_wodexinxi";
+            this.button_wodexinxi.Size = new System.Drawing.Size(120, 40);
+            this.button_wodexinxi.TabIndex = 0;
+            this.button_wodexinxi.Text = "我的信息";
+            this.button_wodexinxi.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button_wodexinxi.UseVisualStyleBackColor = true;
+            this.button_wodexinxi.Click += new System.EventHandler(this.button_wodexinxi_Click);
+            // 
+            // button_settings
+            // 
+            this.button_settings.FlatAppearance.BorderSize = 0;
+            this.button_settings.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_settings.Location = new System.Drawing.Point(798, 2);
+            this.button_settings.Name = "button_settings";
+            this.button_settings.Size = new System.Drawing.Size(75, 23);
+            this.button_settings.TabIndex = 9;
+            this.button_settings.Text = "设置";
+            this.button_settings.UseVisualStyleBackColor = true;
+            // 
+            // button_exit
+            // 
+            this.button_exit.FlatAppearance.BorderSize = 0;
+            this.button_exit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_exit.Location = new System.Drawing.Point(879, 2);
+            this.button_exit.Name = "button_exit";
+            this.button_exit.Size = new System.Drawing.Size(75, 23);
+            this.button_exit.TabIndex = 10;
+            this.button_exit.Text = "退出";
+            this.button_exit.UseVisualStyleBackColor = true;
+            this.button_exit.Click += new System.EventHandler(this.button_exit_Click);
+            // 
+            // panel_wodexinxi
+            // 
+            this.panel_wodexinxi.Controls.Add(this.label1);
+            this.panel_wodexinxi.Location = new System.Drawing.Point(171, 48);
+            this.panel_wodexinxi.Name = "panel_wodexinxi";
+            this.panel_wodexinxi.Size = new System.Drawing.Size(783, 509);
+            this.panel_wodexinxi.TabIndex = 11;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(345, 228);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // panel_jiudehuiyi
+            // 
+            this.panel_jiudehuiyi.Controls.Add(this.label2);
+            this.panel_jiudehuiyi.Location = new System.Drawing.Point(171, 48);
+            this.panel_jiudehuiyi.Name = "panel_jiudehuiyi";
+            this.panel_jiudehuiyi.Size = new System.Drawing.Size(783, 509);
+            this.panel_jiudehuiyi.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(345, 213);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 15);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "label2";
+            // 
+            // panel_xuexiniuren
+            // 
+            this.panel_xuexiniuren.Controls.Add(this.label3);
+            this.panel_xuexiniuren.Location = new System.Drawing.Point(171, 48);
+            this.panel_xuexiniuren.Name = "panel_xuexiniuren";
+            this.panel_xuexiniuren.Size = new System.Drawing.Size(783, 509);
+            this.panel_xuexiniuren.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(345, 213);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 15);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "label3";
+            // 
+            // main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(966, 569);
+            this.Controls.Add(this.panel_jiudehuiyi);
+            this.Controls.Add(this.panel_xuexiniuren);
+            this.Controls.Add(this.panel_wodexinxi);
+            this.Controls.Add(this.button_exit);
+            this.Controls.Add(this.button_settings);
+            this.Controls.Add(this.panel_menu);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "main";
+            this.Text = "谁与争锋";
+            this.Load += new System.EventHandler(this.main_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.main_MouseDown);
+            this.panel_menu.ResumeLayout(false);
+            this.panel_wodexinxi.ResumeLayout(false);
+            this.panel_wodexinxi.PerformLayout();
+            this.panel_jiudehuiyi.ResumeLayout(false);
+            this.panel_jiudehuiyi.PerformLayout();
+            this.panel_xuexiniuren.ResumeLayout(false);
+            this.panel_xuexiniuren.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel_menu;
+        private System.Windows.Forms.Button button_xuexiniuren;
+        private System.Windows.Forms.Button button_jiudehuiyi;
+        private System.Windows.Forms.Button button_wodexinxi;
+        private System.Windows.Forms.Button button_jiantixitong;
+        private System.Windows.Forms.Button button_niurenduibi;
+        private System.Windows.Forms.Button button_wodeliangdian;
+        private System.Windows.Forms.Button button_zuotifenlei;
+        private System.Windows.Forms.Button button_fendoushi;
+        private System.Windows.Forms.Button button_settings;
+        private System.Windows.Forms.Button button_exit;
+        private System.Windows.Forms.Panel panel_wodexinxi;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel_jiudehuiyi;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel_xuexiniuren;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Prototype2.0/Prototype2.0/main.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Drawing.Drawing2D;
+namespace Prototype2._0
+{
+    public partial class main : Form
+    {
+        public main()
+        {
+            InitializeComponent();
+        }
+        //窗口载入时调用
+        private void main_Load(object sender, EventArgs e)
+        {
+            
+            InitMenu();
+            RefleshMenu();
+            InitPanel();
+            RefleshPanel();
+
+        }
+        //无边框窗口拖动代码
+        [DllImport("user32.dll")]
+        public static extern bool ReleaseCapture();
+        [DllImport("user32.dll")]
+        public static extern bool SendMessage(IntPtr hwnd, int wMsg, int wParam, int lParam);
+        public const int WM_SYSCOMMAND = 0x0112;
+        public const int SC_MOVE = 0xF010;
+        public const int HTCAPTION = 0x0002;
+        private void main_MouseDown(object sender, MouseEventArgs e)
+        {
+            ReleaseCapture();
+            SendMessage(this.Handle, WM_SYSCOMMAND, SC_MOVE + HTCAPTION, 0);
+        }
+        
+        //左侧菜单项集合
+        private Dictionary<Button, List<Button>> menu = new Dictionary<Button, List<Button>>();
+        private Dictionary<Button, List<Button>>.KeyCollection firstMenu;
+        //Panel集合
+        private List<Panel> panel = new List<Panel>();
+        //设置子菜单按钮高度
+        private readonly int buttonHeight = 23;
+        //记录已展开的菜单项
+        private int menuOpen = -1;
+        //初始化左侧菜单项
+        private void InitMenu()
+        {
+            this.menu.Add(this.button_wodexinxi, new List<Button>());
+            this.menu.Add(this.button_jiudehuiyi, new List<Button>());
+            this.menu.Add(this.button_xuexiniuren, new List<Button>());
+            this.menu[this.button_jiudehuiyi].Add(this.button_fendoushi);
+            this.menu[this.button_jiudehuiyi].Add(this.button_zuotifenlei);
+            this.menu[this.button_jiudehuiyi].Add(this.button_wodeliangdian);
+            this.menu[this.button_xuexiniuren].Add(this.button_niurenduibi);
+            this.menu[this.button_xuexiniuren].Add(this.button_jiantixitong);
+            firstMenu = this.menu.Keys;
+        }
+        //初始化Panel
+        private void InitPanel()
+        {
+            this.panel.Add(this.panel_wodexinxi);
+            this.panel.Add(this.panel_jiudehuiyi);
+            this.panel.Add(this.panel_xuexiniuren);
+        }
+        //刷新左侧菜单项
+        private void RefleshMenu()
+        {
+            int top = this.panel_menu.Top + 10;
+            foreach (KeyValuePair<Button, List<Button>> menuItem in this.menu)
+            {
+                menuItem.Key.Top = top;
+                top += menuItem.Key.Height + 10;
+                foreach (Button button in menuItem.Value)
+                {
+                    button.Hide();
+                }
+            }
+        }
+        //左侧菜单项点击事件处理
+        private void MenuClick(int index)
+        {
+            if (this.menuOpen != index)
+            {
+                RefleshMenu();
+                OpenMenu(index);
+                menuOpen = index;
+            }
+            else
+            {
+                RefleshMenu();
+                menuOpen = -1;
+            }
+        }
+        //展开指定菜单项
+        private void OpenMenu(int index)
+        {
+            for (int i = index + 1; i < menu.Count; i++)
+            {
+                this.firstMenu.ElementAt(i).Top += this.menu[this.firstMenu.ElementAt(index)].Count * (this.buttonHeight + 10);
+            }
+            int top = this.firstMenu.ElementAt(index).Top + this.firstMenu.ElementAt(index).Height + 10;
+            foreach (Button button in this.menu[this.firstMenu.ElementAt(index)])
+            {
+                button.Top = top;
+                top += this.buttonHeight + 10;
+                button.Show();
+            }
+        }
+        
+        //刷新Panel
+        private void RefleshPanel()
+        {
+            foreach (Panel panel in this.panel)
+            {
+                panel.Hide();
+            }
+            this.panel[0].Show();
+        }
+        //显示index指定的panel
+        private void ShowPanel(int index)
+        {
+            foreach (Panel panel in this.panel)
+            {
+                panel.Hide();
+            }
+            this.panel[index].Show();
+        }
+        private void button_wodexinxi_Click(object sender, EventArgs e)
+        {
+            MenuClick(0);
+            ShowPanel(0);
+        }
+
+        private void button_jiudehuiyi_Click(object sender, EventArgs e)
+        {
+            MenuClick(1);
+            ShowPanel(1);
+        }
+
+        private void button_xuexiniuren_Click(object sender, EventArgs e)
+        {
+            MenuClick(2);
+            ShowPanel(2);
+        }
+
+        
+
+        private void button_exit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        
+
+          
+
+
+        
+    }
+}
